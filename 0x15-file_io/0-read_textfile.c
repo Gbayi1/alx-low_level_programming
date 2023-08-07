095c8769332b345a63fb28a7fa203769576d2cb3
Hudu Abubakar Adegbayi <Abuscogbayi@gmail.com>
2023-08-07 12:24:57 -0700

    Newly Adjusted

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,40 +1,39 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 
-ssize_t read_textfile(const char *filename, size_t letters) {
-    if (filename == NULL) {
-        return 0;
-    }
 
-    int fd = open(filename, O_RDONLY);
-    if (fd == -1) {
-        return 0;
-    }
+/**
+  * read_textfile - reads a text file and prints it to the POSIX standard out
+  * @filename: name of the file to read
+  * @letters: number of characters to print
+  * Return: 0 on success
+  **/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int txt_file, total, read_status;
+	char buffer[BUFSIZE];
 
-    char *buffer = (char *)malloc(letters + 1); // Allocate space for the buffer
-    if (buffer == NULL) {
-        close(fd);
-        return 0;
-    }
-
-    ssize_t bytesRead = read(fd, buffer, letters); // Read from the file
-    if (bytesRead == -1) {
-        free(buffer);
-        close(fd);
-        return 0;
-    }
-
-    ssize_t bytesWritten = write(STDOUT_FILENO, buffer, bytesRead); // Write to standard output
-    if (bytesWritten == -1 || bytesWritten != bytesRead) {
-        free(buffer);
-        close(fd);
-        return 0;
-    }
-
-    free(buffer);
-    close(fd);
-    return bytesRead;
+	if (filename == NULL)
+		return (0);
+	txt_file = open(filename, O_RDONLY);
+	if (txt_file == -1)
+		return (0);
+	total = 0;
+	read_status = 1;
+	while (letters > BUFSIZE && read_status != 0)
+	{
+		read_status = read(txt_file, buffer, BUFSIZE);
+		write(STDOUT_FILENO, buffer, read_status);
+		total += read_status;
+		letters -= BUFSIZE;
+	}
+	read_status = read(txt_file, buffer, letters);
+	write(STDOUT_FILENO, buffer, read_status);
+	total += read_status;
+	close(txt_file);
+	return (total);
 }
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,64 +1,47 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 
-#define BUFFER_SIZE 1024
+/**
+  * main - entry point
+  * @ac: argument count
+  * @av: array of argument tokens
+  * Return: 0 on success
+  */
+int main(int ac, char *av[])
+{
+	int fd_from, fd_to, rd_stat, wr_stat;
+	mode_t perm = S_IRUSR | S_IWUSR | S_IWGRP | S_IRGRP | S_IROTH;
+	char buffer[BUFSIZE];
 
-void print_error_and_exit(int code, const char* format, const char* arg) {
-    dprintf(STDERR_FILENO, format, arg);
-    exit(code);
-}
-
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        print_error_and_exit(97, "Usage: %s file_from file_to\n", argv[0]);
-    }
-
-    const char* file_from = argv[1];
-    const char* file_to = argv[2];
-
-    // Open the source file for reading
-    int fd_from = open(file_from, O_RDONLY);
-    if (fd_from == -1) {
-        print_error_and_exit(98, "Error: Can't read from file %s\n", file_from);
-    }
-
-    // Create or open the destination file for writing, truncate it if it already exists
-    int fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-    if (fd_to == -1) {
-        close(fd_from);
-        print_error_and_exit(99, "Error: Can't write to %s\n", file_to);
-    }
-
-    // Copy data from source to destination using a buffer
-    char buffer[BUFFER_SIZE];
-    ssize_t bytesRead, bytesWritten;
-    while ((bytesRead = read(fd_from, buffer, sizeof(buffer))) > 0) {
-        bytesWritten = write(fd_to, buffer, bytesRead);
-        if (bytesWritten == -1) {
-            close(fd_from);
-            close(fd_to);
-            print_error_and_exit(99, "Error: Can't write to %s\n", file_to);
-        }
-    }
-
-    if (bytesRead == -1) {
-        close(fd_from);
-        close(fd_to);
-        print_error_and_exit(99, "Error: Can't read from file %s\n", file_from);
-    }
-
-    // Close file descriptors
-    if (close(fd_from) == -1) {
-        print_error_and_exit(100, "Error: Can't close fd %d\n", fd_from);
-    }
-
-    if (close(fd_to) == -1) {
-        print_error_and_exit(100, "Error: Can't close fd %d\n", fd_to);
-    }
-
-    return 0;
+	if (ac != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	fd_from = open(av[1], O_RDONLY);
+	if (fd_from == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, perm);
+	if (fd_to == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+	rd_stat = 1;
+	while (rd_stat)
+	{
+		rd_stat = read(fd_from, buffer, BUFSIZE);
+		if (rd_stat == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+		if (rd_stat > 0)
+		{
+			wr_stat = write(fd_to, buffer, rd_stat);
+			if (wr_stat != rd_stat || wr_stat == -1)
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+		}
+	}
+	if (close(fd_from) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+	if (close(fd_to) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+	return (0);
 }
